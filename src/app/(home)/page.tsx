986727621dbda2b3d30506354d47396eed910ac7
Jek08 <jk.pratama86@gmail.com>
2025-02-17 18:51:43 +0700

    Start the project

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+export default function HomePage() {
+  return (
+    <>
+      {/* <HeaderPrimary /> */}
+      <main>
+        {/* <TopicOverview /> */}
+      </main>
+      {/* <FooterPrimary /> */}
+    </>
+  );
+}
