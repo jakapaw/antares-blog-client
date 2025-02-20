f361eca8d1f84b989be6f8c6a58e76ef0983aa13
Jek08 <jk.pratama86@gmail.com>
2025-02-20 14:27:54 +0700

    Implement Author Page

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,5 +1,8 @@
+'use client';
+
 import FooterPrimary from "@/components/FooterPrimary";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import TempImage from "../../../public/blue-ridge-2.jpg";
 import TempProfile from "../../../public/profile-1.jpg";
 import HeaderSecondary from "./HeaderSecondary";
@@ -19,7 +22,7 @@ export default function ArticlePage() {
         <div>
           {/* Tags */}
         </div>
-        <div className="text-justify">
+        <div className="">
           <h2 className="text-lg font-medium">Ringkasan</h2>
           <ul className="list-disc px-2 list-inside *:p-1 text-sm">
             <li>Ut arcu senectus posuere. Per maecenas conubia porttitor aptent laoreet euismod curabitur.</li>
@@ -44,11 +47,13 @@ export default function ArticlePage() {
 }
 
 function WriterInfo() {
+  const navigate = useRouter();
+
   return (
     <div className="h-[200px] p-4">
       <h1 className="font-medium text-lg">Tentang Penulis</h1>
       <div className="flex items-center mt-4">
-        <Image src={TempProfile} alt="Image" className="aspect-square object-cover w-20 h-20" />
+        <Image src={TempProfile} alt="Image" className="aspect-square object-cover w-20 h-20" onClick={() => navigate.push('/author')}/>
         <div className="ml-2 w-2/3">
           <span className="font-medium">Nama Lengkap Penulis</span>
           <p className="text-xs">venenatis iaculis nisl, sit dictum interdum. Phasellus  platea et nunc id potenti orci justo egestas. Velit eget venenatis  iaculis odio per cursus purus nulla elit.</p>
--- src/app/author/page.tsx
+++ src/app/author/page.tsx
@@ -1,5 +1,65 @@
+import { HomeIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
+import TempProfile from "../../../public/profile-1.jpg";
+
 export default function AuthorPage() {
   return (
-    <div>Author Page</div>
+    <>
+      <div className="flex flex-col p-8 gap-4" 
+      style={{
+        backgroundImage: 'url(/profile-bg.png)',
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: '50% 110%'
+      }}>
+        <NavigationBar />
+        <Image src={TempProfile} alt="Profile Image" className="mt-2 self-center aspect-square object-cover rounded-full w-1/2"/>
+        <span className="text-2xl text-white font-bold self-center">Frances Cordova</span>
+      </div>
+      <div className="flex flex-col items-center p-8 gap-8">
+        <div>
+          <h1 className="text-xl font-semibold mb-2">Tentang Saya</h1>
+          <p className="text-justify">venenatis iaculis nisl, sit dictum interdum. Phasellus  platea et nunc id potenti orci justo egestas. Velit eget venenatis  iaculis odio per cursus purus nulla elit.</p>
+        </div>
+        <div className="text-center">
+          <h1 className="text-xl font-semibold mb-4">Kontak & Media Sosial</h1>
+          <div className="grid grid-cols-2 gap-4">
+            <ContactLink name={"Email"} href={""} />
+            <ContactLink name={"LinkedIn"} href={""} />
+            <ContactLink name={"Instagram"} href={""} />
+            <ContactLink name={"X"} href={""} />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+function NavigationBar() {
+  return (
+    <div>
+      <Link href="/">
+        <HomeIcon className="inline-block mr-2 size-8 bg-[#e9e9e9] p-1 text-cobalt rounded-sm"/>
+        <span className="text-sm underline text-white">Kembali ke Halaman Utama</span>
+      </Link>
+    </div>
+  )
+}
+
+function ContactLink({
+  name,
+  logo,
+  href = ""
+}: {
+  name: string,
+  logo?: string,
+  href: string
+}) {
+  return (
+    <Link href={href}>
+      <div className="mx-auto size-[40px] bg-slate-300 rounded-full"></div>
+      <span className="text-sm">{name}</span>
+    </Link>
   )
 }
