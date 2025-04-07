c4201e87c113a2c7270978ef4a95d5e2fa19c6c1
Jek08 <jk.pratama86@gmail.com>
2025-04-07 09:47:31 +0700

    UI updates

--- src/app/articles/[slug]/AuthorInfo.tsx
+++ src/app/articles/[slug]/AuthorInfo.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { SERVER_URL } from "@/lib/config";
+import ProfilePhoto from "@/components/ProfilePhoto";
 import Author from "@/model/author";
 import { LinkedinOutlined } from "@ant-design/icons";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -15,29 +14,31 @@ export default function AuthorInfo({
   return (
     <>
       {
-        authors.map((author) => 
+        authors.map((author) =>
           <div key={author.id} className="flex mb-4 md:mr-10 items-start max-w-[500px]">
-            <Image 
-              src={`${SERVER_URL}${author.profile_photo.url}`} 
-              alt={author.profile_photo.alternativeText || ""} 
-              width={author.profile_photo.width}
-              height={author.profile_photo.height}
-              className="aspect-square object-cover max-w-28 pt-1" 
-              onClick={() => navigate.push(`/authors/${author.username}`)}/>
+              <ProfilePhoto
+                src={author.profile_photo?.url}
+                alt={author.profile_photo?.alternativeText}
+                width={author.profile_photo?.width}
+                height={author.profile_photo?.height}
+                className="aspect-square object-cover size-[80px] mt-1 rounded-full"
+                onClick={() => navigate.push(`/authors/${author.username}`)} />
             <div className="ml-3">
               <span className="font-medium">
-                {author.fullname}
-                <Link 
+                <span className="text-lg md:text-2xl">{author.fullname}</span>
+                <Link
                   href={
-                    author.social_media.find((el) => el.social_media_name.toLowerCase() == "linkedin")?.link
-                    || ""
+                    author.social_media?.find((el) => el.social_media_name.toLowerCase() == "linkedin")?.link
+                    ?? ""
                   }>
-                  <LinkedinOutlined className="ml-2 text-[#0A66C2] text-xl"/>
+                  <LinkedinOutlined className="ml-2 text-[#0A66C2] text-xl" />
                 </Link>
+                <br/>
+                <span className="text-sm md:text-lg">{author.headline}</span>
               </span>
-              <p className="text-xs pt-2">{author.profile_summary}</p>
+              <p className="text-xs md:text-base pt-2">{author.profile_summary}</p>
             </div>
-          </div>   
+          </div>
         )
       }
     </>
