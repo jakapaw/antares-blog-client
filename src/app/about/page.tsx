43c29276d4802ff68a02f3df90aa49e89c2d4ad2
Jek08 <jk.pratama86@gmail.com>
2025-04-10 01:48:00 +0700

    updates

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,13 @@
 'use client';
 import FooterSecondary from "@/components/FooterSecondary";
 import HomeNavigationBar from "@/components/HomeNavigationBar";
+import ProfilePhoto from "@/components/ProfilePhoto";
 import Spacer from "@/components/Spacer";
-import { SERVER_URL } from "@/lib/config";
 import { getBrandInfo } from "@/lib/data";
 import Author from "@/model/author";
 import Brand from "@/model/brand";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 class AboutPageState {
@@ -39,18 +40,18 @@ export default function AboutUsPage() {
           <HomeNavigationBar />
         </span>
         <div className="bg-blue-950 px-14 py-6 text-white">
-          <h1 className="text-[62px] md:text-[110px] font-bold">Antares</h1>
-          <h2 className="text-lg md:text-2xl -mt-4">Reach The Skies and Beyond</h2>
+          <h1 className="text-[55px] md:text-[110px] font-bold">Antares</h1>
+          <h2 className="md:text-2xl -mt-4">Reach The Skies and Beyond</h2>
           <p className="text-sm md:text-lg my-5 md:my-10">{state.brand.introduction}</p>
         </div>
       </div>
       <Spacer />
-      <div className="flex flex-col items-center px-12 mb-10">
+      <div className="flex flex-col items-center px-5 mb-10">
         <span>
           <Image src="deco-1.svg" width={80} height={80} alt="" className="relative -left-8 size-[60px] md:size-[80px]" />
-          <h1 className="font-extrabold text-3xl md:text-5xl">Our Best Team</h1>
+          <h1 className="font-extrabold text-3xl md:text-5xl">Best Team</h1>
         </span>
-        <div className="mt-8 grid grid-cols-2 md:grid-cols-3 gap-6 md:w-2/3">
+        <div className="mt-8 flex flex-wrap justify-center gap-6 md:w-1/2">
           {
             state.brand.authors.map((el, i) =>
               <TeamMember key={i} author={el} />
@@ -69,16 +70,28 @@ function TeamMember({
 }: {
   author: Author
 }) {
+  const navigate = useRouter();
+
+  let name = author.fullname;
+  if (name.length > 12) {
+    const split = name.split(" ");
+    const first = split.slice(0, 2);
+    let last = split.slice(2);
+    last = last.map(el => (el.at(0) ?? "") + ".");
+    name = [first, last].flatMap(el => el).join(" ");
+  }
+  
   return (
     <div className="flex flex-col items-center">
-      <Image
-        src={`${SERVER_URL}${author.profile_photo.url}`}
-        width={author.profile_photo.width}
-        height={author.profile_photo.height}
+      <ProfilePhoto
+        src={author.profile_photo?.url}
+        width={author.profile_photo?.width}
+        height={author.profile_photo?.height}
         alt={`${author.fullname}'s photo`}
-        className="rounded-full aspect-square object-cover p-2 w-[100px] md:w-[180px]" />
-      <span className="font-bold text-black text-sm md:text-xl">{author.fullname}</span>
-      <span className="text-xs md:text-base">{author.headline}</span>
+        className="rounded-full aspect-square object-cover p-2 w-[100px] md:w-[180px]"
+        onClick={() => navigate.push(`/authors/${author.username}`)} />
+      <span className="font-bold text-black text-[10px] md:text-xl">{name}</span>
+      <span className="text-[9px] md:text-base">{author.headline}</span>
     </div>
   )
 }
--- src/app/articles/[slug]/AuthorInfo.tsx
+++ src/app/articles/[slug]/AuthorInfo.tsx
@@ -1,8 +1,7 @@
 'use client';
-import { SERVER_URL } from "@/lib/config";
+import ProfilePhoto from "@/components/ProfilePhoto";
 import Author from "@/model/author";
 import { LinkedinOutlined } from "@ant-design/icons";
-import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 
@@ -17,11 +16,11 @@ export default function AuthorInfo({
       {
         authors.map((author) =>
           <div key={author.id} className="flex mb-4 md:mr-10 items-start max-w-[500px]">
-              <Image
-                src={`${SERVER_URL}${author.profile_photo.url}`}
-                alt={author.profile_photo.alternativeText || ""}
-                width={author.profile_photo.width}
-                height={author.profile_photo.height}
+              <ProfilePhoto
+                src={author.profile_photo?.url}
+                alt={author.profile_photo?.alternativeText}
+                width={author.profile_photo?.width}
+                height={author.profile_photo?.height}
                 className="aspect-square object-cover size-[80px] mt-1 rounded-full"
                 onClick={() => navigate.push(`/authors/${author.username}`)} />
             <div className="ml-3">
@@ -29,8 +28,8 @@ export default function AuthorInfo({
                 <span className="text-lg md:text-2xl">{author.fullname}</span>
                 <Link
                   href={
-                    author.social_media.find((el) => el.social_media_name.toLowerCase() == "linkedin")?.link
-                    || ""
+                    author.social_media?.find((el) => el.social_media_name.toLowerCase() == "linkedin")?.link
+                    ?? ""
                   }>
                   <LinkedinOutlined className="ml-2 text-[#0A66C2] text-xl" />
                 </Link>
